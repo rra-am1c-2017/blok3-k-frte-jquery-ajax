c7566c5a86d370c820300b9b04c0278571708e8a
Arjan de Ruijter <rra.am1a.2017@gmail.com>
2018-02-02 16:17:16 +0100

    Einde lesweek 1

--- selectors_and_filters/js/script.js
+++ selectors_and_filters/js/script.js
@@ -14,16 +14,18 @@ $(document).ready(function() {
                   "padding" : "25px",
                   "margin" : "5px"};
 
+  // We selecteren een li tag en passen de css in een js object toe
   $("li").css(style_li);
 
-
+  // We selecteren een tag met id="left" en passen css toe in een js object
   $("#left").css({"background-color" : "rgb(220, 220, 220)",
                   "color" : "white",
                   "font-size" : "24px"});
-                  
+   
+  // We selecteren een tag met id="right" en passen css toe in een js object
   $("#right").css({"background-color" : "rgb(200, 200, 200)"});
-  
-
 });
+
+// Einde lesweek 1
 
 
