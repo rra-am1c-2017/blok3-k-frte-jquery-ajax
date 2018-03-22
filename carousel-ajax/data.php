20640dbd2eb700269c6d1eb893fed8d0fb4607f1
Arjan de Ruijter <rra.am1a.2017@gmail.com>
2018-03-22 12:42:16 +0100

    ajax finished

--- carousel-ajax/data.php
+++ carousel-ajax/data.php
@@ -1,3 +1,16 @@
 <?php
+  // Maak contact met de mysql-server
   include("./connect_db.php");
+
+  // Maak de sql-query
+  $sql = "SELECT * FROM `ajax` WHERE `category` = '{$_POST["category"]}'";
+
+  // Vuur de query af op de database
+  $result = mysqli_query($conn, $sql);
+
+  // Zet het resultaat om naar een associatief array
+  $records = mysqli_fetch_all($result, MYSQLI_ASSOC);
+
+  // Zet het associatieve array om naar een array van javascriptobjecten
+  echo json_encode($records);
 ?>
--- carousel-ajax/js/script.js
+++ carousel-ajax/js/script.js
@@ -1,5 +1,6 @@
 // Dit zorgt ervoor dat de pagina index.html helemaal geladen is voordat we scripten
 $(document).ready(function () {
+  ajaxCall("fruit");
 
   // We selectoren de select-tag
   $("#selectCategory").on("change", function () {
@@ -7,13 +8,22 @@ $(document).ready(function () {
     // Dit is de gekozen waarde in de select
     var category = $(this).val();
 
-    $.post("http://localhost/2017-2018/blok3/k-frte-am1c/carousel-ajax/data.php",
-           {"category" : category},
-           function(data) {
-            // Hier komt code om de opgehaalde tekst weg te zetten op index.html
-            console.log(data);
-            $(".jumbotron p").html(data);
-           },
-           "text");
+    ajaxCall(category);
   })
+
+
+  function ajaxCall(value) {
+    $.post("http://localhost/2017-2018/blok3/k-frte-am1c/carousel-ajax/data.php",
+    {"category" : value},
+    function(data) {
+     // Hier komt code om de opgehaalde tekst weg te zetten op index.html
+     for ( var i = 0; i < data.length; i++) {
+       console.log(data[i].name);
+       $(".carousel-inner img").eq(i).attr("src" , "./images/" + data[i].name);
+       $(".carousel-inner h5").eq(i).html(data[i].title);
+       $(".carousel-inner p").eq(i).html(data[i].info);
+     }
+    },
+    "json");
+  }
 });
